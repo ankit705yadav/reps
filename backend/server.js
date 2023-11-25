9b6b46add9294053643735d4f6424b82e5529b01
ankit705yadav <ankit705yadav@gmail.com>
2023-11-25 13:13:06 +0530

    done routing #3

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 //express app
 const app = express();
 
 //midleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ message: "hello" });
-});
+//routes
+app.use("/api/workouts", workoutRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("listening on port", process.env.PORT);
