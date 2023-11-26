66fdb7bf6e56c6f23379fb5bf65501b85f860865
ankit705yadav <ankit705yadav@gmail.com>
2023-11-26 17:58:02 +0530

    #6

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,20 @@
 const express = require("express");
-const Workout = require("../models/workoutModel");
+const {
+  getWorkouts,
+  getWorkout,
+  createworkout,
+} = require("../controllers/workoutControllers");
 
 const router = express.Router();
 
 //Get all workoutes
-router.get("/", (req, res) => {
-  res.json({ message: "Get all workoutes" });
-});
+router.get("/", getWorkouts);
 
 //Get single workout
-router.get("/:id", (req, res) => {
-  res.json({ message: "Get a single workout" });
-});
+router.get("/:id", getWorkout);
 
 //Post a new workout
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+router.post("/", createworkout);
 
 //Delete a workout
 router.delete("/:id", (req, res) => {
