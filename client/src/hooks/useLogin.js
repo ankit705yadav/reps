9d1f4a8b5e05e94a551c0a9b2ebbf2168d5fd35d
ankit705yadav <ankit705yadav@gmail.com>
2023-12-08 03:07:58 +0530

    hardCoded route

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -10,11 +10,14 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("/api/user/login", {
-      method: "POST",
-      headers: { "content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+    const response = await fetch(
+      "https://reps-server.vercel.app/api/user/login",
+      {
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      }
+    );
     const json = await response.json();
 
     if (!response.ok) {
